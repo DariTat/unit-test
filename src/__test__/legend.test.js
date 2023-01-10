aea6513e0f97e91add716539ec4cf7dbbad5ce9d
Daria Tatarinova <dt.dashenka@mail.ru>
2023-01-10 18:36:12 +1000

    init

---
+++ src/__test__/legend.test.js
@@ -0,0 +1,14 @@
+import displayLife from '../legend';
+
+test('show healthy', () => {
+  const result = displayLife({ name: 'Маг', health: 90 });
+  expect(result).toBe('healthy');
+});
+test('show wounded', () => {
+  const result = displayLife({ name: 'Гном', health: 35 });
+  expect(result).toBe('wounded');
+});
+test('show critical', () => {
+  const result = displayLife({ name: 'Ученик', health: 10 });
+  expect(result).toBe('critical');
+});
